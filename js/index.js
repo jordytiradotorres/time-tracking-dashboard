b4ab97d1b8098f244d683acd7a766cb3732a0bf8
Jordy Tirado Torres <jordytiradotorres20@gmail.com>
2021-09-15 11:44:31 -0500

    interacting with javascript

--- js/index.js
+++ js/index.js
@@ -38,12 +38,11 @@ const fetchData = async () => {
   }
 };
 
-const data = fetchData();
-data.then((result) => {
-  console.log(result);
-  result.map((item, index) => {
-    const template = `
-        <div class="item ${item.title.toLowerCase().replaceAll(" ", "-")}">
+const showCard = (item, index, value) => {
+  const hours = item.timeframes[value].current;
+  const lastWeek = item.timeframes[value].previous;
+
+  const template = `
           <figure class="item__image">
             <img src=${images[index].url} alt=${images[index].title} />
           </figure>
@@ -55,13 +54,108 @@ data.then((result) => {
             </div>
 
             <div class="item__date">
-              <time>${item.timeframes.weekly.current}hrs</time>
-              <p>Last Week - ${item.timeframes.weekly.previous}hrs</p>
+              <time>${hours}hrs</time>
+              <p>Last Week - ${lastWeek}hrs</p>
             </div>
           </div>
-        </div>
     `;
 
-    containerItems.insertAdjacentHTML("beforeend", template);
+  return template;
+};
+
+const initialCards = (value) => {
+  return `
+  <div class="dashboard__item">
+    <div class="avatar">
+      <img src="./images/image-jeremy.png" alt="jeremy" />
+      <span>Report for</span>
+      <h3>Jeremy Robson</h3>
+    </div>
+
+    <div class="report">
+      <ul class="report__list">
+      ${
+        value === "daily"
+          ? `<li>
+            <a href="#" class="daily active">Daily</a>
+          </li>
+          <li>
+            <a href="#" class="weekly">Weekly</a>
+          </li>
+          <li>
+            <a href="#" class="monthly">Monthly</a>
+          </li>`
+          : ""
+      }
+      ${
+        value === "weekly"
+          ? `<li>
+            <a href="#" class="daily">Daily</a>
+          </li>
+          <li>
+            <a href="#" class="weekly active">Weekly</a>
+          </li>
+          <li>
+            <a href="#" class="monthly">Monthly</a>
+          </li>`
+          : ""
+      }
+      ${
+        value === "monthly"
+          ? `<li>
+            <a href="#" class="daily">Daily</a>
+          </li>
+          <li>
+            <a href="#" class="weekly">Weekly</a>
+          </li>
+          <li>
+            <a href="#" class="monthly active">Monthly</a>
+          </li>`
+          : ""
+      }
+      </ul>
+    </div>
+</div>`;
+};
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  let value = "weekly";
+
+  const data = fetchData();
+  data.then((result) => {
+    const fragment = document.createDocumentFragment();
+
+    result.map((item, index) => {
+      const restContent = document.createElement("div");
+      restContent.classList.add("item");
+      restContent.innerHTML = showCard(item, index, value);
+
+      fragment.appendChild(restContent);
+    });
+
+    containerItems.appendChild(fragment);
   });
+});
+
+document.addEventListener("click", (e) => {
+  if (e.target.tagName === "A") {
+    const reportListValue = e.target.textContent.toLowerCase().trim();
+    const fragment = document.createDocumentFragment();
+
+    let value = e.target.classList.value;
+
+    const data = fetchData();
+    data.then((result) => {
+      containerItems.innerHTML = initialCards(value);
+
+      result.map((item, index) => {
+        const restContent = document.createElement("div");
+        restContent.classList.add("item");
+        restContent.innerHTML = showCard(item, index, reportListValue);
+
+        fragment.appendChild(restContent);
+      });
+      containerItems.appendChild(fragment);
+    });
+  }
 });
